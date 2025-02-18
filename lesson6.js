1a8296fa2e8dd145b354446b48ed9b026ab2b85b
Radosław Grzanka <r.grzanka@ks-software.com>
2025-02-18 07:57:25 +0000

    Lesson6 - validating and submitting form

---
+++ lesson6.js
@@ -0,0 +1,45 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('signupForm', () => ({
+        username: '',
+        email: '',
+        password: '',
+        passwordConfirm: '',
+        belt: '',
+        bio: '',
+        newsletter: true,
+        showPass: false,
+        errors: {},
+
+        validateForm() {
+            this.errors = {}
+            if (this.username.length < 3) {
+                this.errors.username = 'Username must be at least 3 characters long'
+            }
+            if (!this.email.includes('@')) {
+                this.errors.email = 'Please enter a valid email address'
+            }
+            if (this.password.length < 8) {
+                this.errors.password = 'Password must be at least 8 characters long'
+            }
+            if (this.password !== this.passwordConfirm) {
+                this.errors.passwordConfirm = 'Passwords do not match'
+            }
+            if (!this.belt) {
+                this.errors.belt = 'Please select a belt color'
+            }
+            if (this.bio.length < 10) {
+                this.errors.bio = 'Bio must be at least 10 characters long'
+            }
+        },
+
+        submitForm($event) {
+            this.validateForm()
+            console.log(this.errors)
+            if (Object.keys(this.errors).length === 0) {
+                console.log(this.username, this.email, this.password, this.passwordConfirm, this.belt, this.bio, this.newsletter)
+
+                $event.target.reset()
+            }
+        }
+    }))
+})
