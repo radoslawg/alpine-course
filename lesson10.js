4d6d149f4714c054051fced8b51bac6d2ef76b24
Radosław Grzanka <radoslawg@gmail.com>
2025-02-26 10:48:31 +0000

    Bring back validations

--- lesson10.js
+++ lesson10.js
@@ -14,24 +14,24 @@ document.addEventListener('alpine:init', () => {
 
         validateForm() {
             this.errors = {}
-            // if (this.username.length < 3) {
-            //     this.errors.username = 'Username must be at least 3 characters long'
-            // }
-            // if (!this.email.includes('@')) {
-            //     this.errors.email = 'Please enter a valid email address'
-            // }
-            // if (this.password.length < 8) {
-            //     this.errors.password = 'Password must be at least 8 characters long'
-            // }
-            // if (this.password !== this.passwordConfirm) {
-            //     this.errors.passwordConfirm = 'Passwords do not match'
-            // }
-            // if (!this.belt) {
-            //     this.errors.belt = 'Please select a belt color'
-            // }
-            // if (this.bio.length < 10) {
-            //     this.errors.bio = 'Bio must be at least 10 characters long'
-            // }
+            if (this.username.length < 3) {
+                this.errors.username = 'Username must be at least 3 characters long'
+            }
+            if (!this.email.includes('@')) {
+                this.errors.email = 'Please enter a valid email address'
+            }
+            if (this.password.length < 8) {
+                this.errors.password = 'Password must be at least 8 characters long'
+            }
+            if (this.password !== this.passwordConfirm) {
+                this.errors.passwordConfirm = 'Passwords do not match'
+            }
+            if (!this.belt) {
+                this.errors.belt = 'Please select a belt color'
+            }
+            if (this.bio.length < 10) {
+                this.errors.bio = 'Bio must be at least 10 characters long'
+            }
         },
 
         submitForm($event) {
